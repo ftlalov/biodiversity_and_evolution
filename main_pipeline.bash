bd08237d6d4a436794cabc3592453a1cfc8a773c
ftlalov <eduardoferrarat@gmail.com>
2025-10-15 12:55:44 -0600

    creación_del_pipeline_principal

---
+++ main_pipeline.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script principal para el análisis de datos para   (pipeline.bash)
+# ----------------------------------------------------------------------
+# Este Script es el pipeline el cual toma secuencias FASTQ 
+# procesa las secuencias, las filtra, y las ingresa a los
+# distintos programas necesarios para darle valor biológico a
+# la información proveninete de la tecnológica de secuenciación masiva
+# ----------------------------------------------------------------------
+
+
+#Verificación de programas necesarios.
+
