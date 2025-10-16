746c88b76ed1273db00cbe293c41f2665e032f9c
ftlalov <eduardoferrarat@gmail.com>
2025-10-16 00:51:25 -0600

    Se añadieron funciones, se esta construyendo el esqueleto del pipeline y se añadio un menú para el ingreso del tipo de metagenoma

--- main_pipeline.bash
+++ main_pipeline.bash
@@ -8,6 +8,59 @@
 # la información proveninete de la tecnológica de secuenciación masiva
 # ----------------------------------------------------------------------
 
+ # Función para la selección del tipo de metagenoma
+seleccionar_tipo_metagenoma() {
+    echo "--- Paso 1: Selección del Tipo de Metagenoma ---"
+    PS3="Selecciona el tipo de análisis (1, 2, 3): "
+    options=("Amplicon (16S/18S)" "Shotgun (Whole Genome Shootgun)" "Salir")
+    select opt in "${options[@]}"; do
+        case $opt in
+            "Amplicon (16S/18S)")
+                METAGENOMA_TIPO="amplicon"
+                echo "Seleccionado: Metagenoma de Amplicon (16S/ITS)."
+                a=1
+                break
+                ;;
+            "Shotgun (Whole Genome Shootgun)")
+                METAGENOMA_TIPO="shotgun"
+                echo "Seleccionado: Metagenoma de Shotgun (WGS)."
+                a=2
+                break
+                ;;
+            "Salir")
+                echo "Saliendo del programa."
+                exit 0
+                ;;
+            *) echo "Opción inválida $REPLY";;
+        esac
+    done
+}
 
-#Verificación de programas necesarios.
+
+
+seleccionar_tipo_metagenoma
+
+####### esqueleto principal 
+
+if [[ $a -eq 1 ]]; then 
+        echo "Iniciando para metagenoma 16S/18S"
+        bash enviroment_verification.bash
+        echo "se Ha completado la verificación"
+
+    elif [[ $a -eq 2 ]]; then 
+        echo "Iniciando para metagenoma Shotgun"
+        bash enviroment_verification.bash
+        echo "se Ha completado la verificación"
+
+
+    fi
+
+
+
+
+
+
+
+
+
 
