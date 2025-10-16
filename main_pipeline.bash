4002d2ea5d9f1d2a3d8ddbdefe4ae3df09f89fd2
ftlalov <eduardoferrarat@gmail.com>
2025-10-16 11:06:28 -0600

    Se agregaron pasos de verificacion

--- main_pipeline.bash
+++ main_pipeline.bash
@@ -7,6 +7,31 @@
 # distintos programas necesarios para darle valor biológico a
 # la información proveninete de la tecnológica de secuenciación masiva
 # ----------------------------------------------------------------------
+config_seq=$1
+archivo_muestras="$2"
+declare -A archivos
+
+
+##Filtro de datos necesarios para acceder al script
+if [ -z "$1" ]; then
+    echo "Error, Debe especificar el tipo de lectura (PE o SE) como primer argumento." >&2
+    echo "Uso: $0 [PE|SE] [archivo_muestras]" >&2
+    exit 1
+fi
+config_seq=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+if [[ "$config_seq" != "PE" && "$config_seq" != "SE" ]]; then
+    echo " Error: El tipo de lectura proporcionado ('$1') no es válido." >&2
+    echo "   Solo se permiten 'PE' (Paired-End) o 'SE' (Single-End)." >&2
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Es necesario ejecutar con el archivo de muestras"
+    echo "Este debe contener el nombre de las muestras"
+    echo "junto con la/las rutas absolutas de los FASTQ"
+    echo "Uso: $0 [PE|SE] [archivo_muestras]"
+    exit 1
+fi
 
  # Función para la selección del tipo de metagenoma
 seleccionar_tipo_metagenoma() {
@@ -36,9 +61,18 @@ seleccionar_tipo_metagenoma() {
     done
 }
 
+verificar_datos() {
+   echo "$archivo_muestras"
+   while read -r ALIAS FILE_PATH; do
+        if [[ ! -z "$ALIAS" && ! -z "$FILE_PATH" ]]; then
+            FILES["$ALIAS"]="$FILE_PATH"
+        fi
+    done < <(awk 'NF{print $1 "\t" $2}' "$SAMPLE_MAPPING_FILE")
 
+}
 
-seleccionar_tipo_metagenoma
+verificar_datos
+#seleccionar_tipo_metagenoma
 
 ####### esqueleto principal 
 
